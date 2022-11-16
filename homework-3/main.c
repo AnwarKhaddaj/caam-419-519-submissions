c978d175cec5a19e7592c9e4db1aded3f9652d81
AnwarKhaddaj <58235746+AnwarKhaddaj@users.noreply.github.com>
2022-11-16 00:02:25 -0600

    Update main.c

--- homework-3/main.c
+++ homework-3/main.c
@@ -10,7 +10,7 @@ int main(void){
   int n = 5;
   Matrix * A = allocate_Matrix(n, n);
 
-  printf("Running Matrix...");
+  printf("Running Matrix...\n");
     for (int i = 0; i < n; ++i){
       for (int j = 0; j < n; ++j){
 	A->ptr[i][j] = (double) (i+j);
@@ -19,17 +19,17 @@ int main(void){
   print_Matrix(A);
 
   DiagonalMatrix * D=DiagonalMatrix_copy(A);
-  printf("Running Diagonal Matrix...");
+  printf("Running Diagonal Matrix...\n");
     print_DiagonalMatrix(D);
   free_DiagonalMatrix(D);
 
   UpperTriangularMatrix* U=UpperTriangularMatrix_copy(A);
-  printf("Running Upper Triangular Matrix...");
+  printf("Running Upper Triangular Matrix...\n");
     print_UpperTriangularMatrix(U);
   free_UpperTriangularMatrix(U);
 
   TridiagonalMatrix* T= TridiagonalMatrix_copy(A);
-  printf("Running Tridiagonal Matrix...");
+  printf("Running Tridiagonal Matrix...\n");
     print_TridiagonalMatrix(T);
   free_TridiagonalMatrix(T);
 
