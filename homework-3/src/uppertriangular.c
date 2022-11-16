d3e7a088807d079f30decb61c5ce7d5c5640de40
AnwarKhaddaj <58235746+AnwarKhaddaj@users.noreply.github.com>
2022-11-15 19:25:28 -0600

    Update uppertriangular.c

--- homework-3/src/uppertriangular.c
+++ homework-3/src/uppertriangular.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrix.h"
-#include "upperdiagonal.h"
+#include "uppertriangular.h"
 
 void free_UpperTriangularMatrix(UpperTriangularMatrix *A){
   free(A->ptr);  
